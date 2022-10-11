594235f371af12b029daa858d921e50fe7835c9f
남지수 <anm0307@naver.com>
2022-10-11 21:06:20 +0900

    #1 - Add UnCommited File & Update File
    none description

--- board/src/main/java/com/fastcampus/projectboard/domain/Article.java
+++ board/src/main/java/com/fastcampus/projectboard/domain/Article.java
@@ -21,9 +21,8 @@ import java.util.Set;
         @Index(columnList = "createdAt"),
         @Index(columnList = "createdBy"),
 })
-@EntityListeners(AuditingEntityListener.class)
 @Entity
-public class Article {
+public class Article extends AuditingFields {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -37,11 +36,6 @@ public class Article {
     @OrderBy("id")
     @OneToMany(mappedBy = "article", cascade = CascadeType.ALL)
     private final Set<ArticleComment> articleComments = new LinkedHashSet<>();
-
-    @CreatedDate @Column(nullable = false) private LocalDateTime createdAt;    // 생성 일시
-    @CreatedBy @Column(nullable = false, length = 100)private String createdBy;           // 생성자
-    @CreatedDate @Column(nullable = false) private LocalDateTime modifiedAt;   // 수정 일시
-    @CreatedBy @Column(nullable = false, length = 100) private String modifiedBy;          // 수정자
 
     protected Article() {}
 
