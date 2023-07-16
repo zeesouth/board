e7b4d16e658e874f995c8862b046e4e27e4e5a3f
남지수 <anm0307@naver.com>
2023-07-17 07:57:18 +0900

    #17 - 회원 계정 도메인 구현 및 erd 반영
    `UserAccount` 이름으로 회원 계정 도메인 생성
    `user`, `account` 등은 mysql 예약어이므로 피함
    필드명도 mysql 예약어를 의식하여 정함
    
    테스트 데이터에 임의 계정 1개 등록
    테스트용이지만 패스워드가 노출되는 방식이므로 고민 필요
    
    회원 정보는 API 로 노출되지 않았으면 한다.
    이에 테스트에 404 not found 를 확인하는 테스트 추가
    간단하게 모든 http 메소드에 대응

--- board/src/test/java/com/fc/board/controller/DataRestTest.java
+++ board/src/test/java/com/fc/board/controller/DataRestTest.java
@@ -1,6 +1,5 @@
 package com.fc.board.controller;
 
-import javax.transaction.Transactional;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -9,76 +8,93 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
-@DisplayName("Spring Data REST 통합테스트는 불필요하므로 제외")
-@Disabled
+@Disabled("Spring Data REST 통합테스트는 불필요하므로 제외시킴")
+@DisplayName("Data REST - API 테스트")
 @Transactional
 @AutoConfigureMockMvc
 @SpringBootTest
 public class DataRestTest {
+
     private final MockMvc mvc;
 
     public DataRestTest(@Autowired MockMvc mvc) {
         this.mvc = mvc;
     }
 
-    @DisplayName("[API] 게시글 리스트 조회")
+
+    @DisplayName("[api] 게시글 리스트 조회")
     @Test
     void givenNothing_whenRequestingArticles_thenReturnsArticlesJsonResponse() throws Exception {
-        // given
+        // Given
 
-        // when & then
+        // When & Then
         mvc.perform(get("/api/articles"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.valueOf("application/hal+json")));
     }
 
-    @DisplayName("[API] 게시글 단건 조회")
+    @DisplayName("[api] 게시글 단건 조회")
     @Test
     void givenNothing_whenRequestingArticle_thenReturnsArticleJsonResponse() throws Exception {
-        // given
+        // Given
 
-        // when & then
+        // When & Then
         mvc.perform(get("/api/articles/1"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.valueOf("application/hal+json")));
     }
 
-    @DisplayName("[API] 게시글 -> 댓글 리스트 조회")
+    @DisplayName("[api] 게시글 -> 댓글 리스트 조회")
     @Test
     void givenNothing_whenRequestingArticleCommentsFromArticle_thenReturnsArticleCommentsJsonResponse() throws Exception {
-        // given
+        // Given
 
-        // when & then
+        // When & Then
         mvc.perform(get("/api/articles/1/articleComments"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.valueOf("application/hal+json")));
     }
 
-    @DisplayName("[API] 댓글 리스트 조회")
+    @DisplayName("[api] 댓글 리스트 조회")
     @Test
     void givenNothing_whenRequestingArticleComments_thenReturnsArticleCommentsJsonResponse() throws Exception {
-        // given
+        // Given
 
-        // when & then
+        // When & Then
         mvc.perform(get("/api/articleComments"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.valueOf("application/hal+json")));
     }
 
-    @DisplayName("[API] 댓글 단건 조회")
+    @DisplayName("[api] 댓글 단건 조회")
     @Test
     void givenNothing_whenRequestingArticleComment_thenReturnsArticleCommentJsonResponse() throws Exception {
-        // given
+        // Given
 
-        // when & then
+        // When & Then
         mvc.perform(get("/api/articleComments/1"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.valueOf("application/hal+json")));
     }
+
+    @DisplayName("[api] 회원 관련 API 는 일체 제공하지 않는다.")
+    @Test
+    void givenNothing_whenRequestingUserAccounts_thenThrowsException() throws Exception {
+        // Given
+
+        // When & Then
+        mvc.perform(get("/api/userAccounts")).andExpect(status().isNotFound());
+        mvc.perform(post("/api/userAccounts")).andExpect(status().isNotFound());
+        mvc.perform(put("/api/userAccounts")).andExpect(status().isNotFound());
+        mvc.perform(patch("/api/userAccounts")).andExpect(status().isNotFound());
+        mvc.perform(delete("/api/userAccounts")).andExpect(status().isNotFound());
+        mvc.perform(head("/api/userAccounts")).andExpect(status().isNotFound());
+    }
+
 }
