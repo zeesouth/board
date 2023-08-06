eb889f401594e9cb62b25a3a9a8d3f07a64fb097
남지수 <anm0307@naver.com>
2023-08-06 21:52:01 +0900

    #44 - UserAccount.java 리팩토링
    리팩토링

--- board/src/main/java/com/fc/board/domain/UserAccount.java
+++ board/src/main/java/com/fc/board/domain/UserAccount.java
@@ -20,7 +20,6 @@ public class UserAccount extends AuditingFields {
     @Column(length = 50)
     private String userId;
 
-    @Setter @Column(nullable = false, length = 50) private String userId;
     @Setter @Column(nullable = false) private String userPassword;
 
     @Setter @Column(length = 100) private String email;
