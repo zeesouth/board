7d6f5820c1e0c4d92c80d8ea608e75a39df81155
남지수 <anm0307@naver.com>
2023-09-08 10:37:02 +0900

    #101 - 대댓글 비즈니스 로직 구현
    부모/자식 관계를 만들면서 entity <-> dto 간 변환 로직,
    댓글 서비스에서 댓글과 대댓글을 저장하고 삭제하는 방법,
    댓글과 대댓글을 db 데이터로부터 변환할 때
    서로 계층 구조를 만들고 각각 정렬하는 과정을 구현

--- board/src/main/java/com/fc/board/service/ArticleCommentService.java
+++ board/src/main/java/com/fc/board/service/ArticleCommentService.java
@@ -1,6 +1,7 @@
 package com.fc.board.service;
 
 import com.fc.board.domain.Article;
+import com.fc.board.domain.ArticleComment;
 import com.fc.board.domain.UserAccount;
 import com.fc.board.dto.ArticleCommentDto;
 import com.fc.board.repository.ArticleCommentRepository;
@@ -35,7 +36,15 @@ public class ArticleCommentService {
         try {
             Article article = articleRepository.getReferenceById(dto.articleId());
             UserAccount userAccount = userAccountRepository.getReferenceById(dto.userAccountDto().userId());
-            articleCommentRepository.save(dto.toEntity(article, userAccount));
+            ArticleComment articleComment = dto.toEntity(article, userAccount);
+
+            if(dto.parentCommentId() != null) {
+                ArticleComment parentComment = articleCommentRepository.getReferenceById(dto.parentCommentId());
+                parentComment.addChildComment(articleComment);
+            } else {
+                articleCommentRepository.save(articleComment);
+            }
+
         } catch (EntityNotFoundException e) {
             log.warn("댓글 저장 실패. 댓글 작성에 필요한 정보를 찾을 수 없습니다 - dto: {}", e.getLocalizedMessage());
         }
